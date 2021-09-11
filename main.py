1686f8f16b4c4e74201857602d8c199e3aab6b91
Awos <uws0331@gmail.com>
2021-09-11 21:07:05 +0900

    주석 정리, 교수님 이름 추가 및 get_text2() 추가

--- main.py
+++ main.py
@@ -7,16 +7,16 @@ from bs4 import BeautifulSoup
 
 os.system("cls")
 
+# env_var
 loginType = os.environ['LOGIN_TYPE']
 loginKind = os.environ['LOGIN_KIND']
 user = os.environ['USERID']
 password = os.environ['PASSWORD']
-
+login_url = os.environ['LOGIN_URL']
 lec_address = os.environ['lec_address']
 course_param1 = os.environ['COURSE_PARAM1']
 course_param2 = os.environ['COURSE_PARAM2']
 
-# print(sess)
 login = {
     'loginType': loginType,
     'loginKind': loginKind,
@@ -29,24 +29,29 @@ headers = {
     "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9"
 }
 
-login_url = os.environ['LOGIN_URL']
 session = requests.session()
-# 로그인을 해야 스크래핑이 가능함.
+# 로그인을 해서 스크래핑이 가능함.
 session.post(url=login_url, data=login)
 
 lec_list = []
 
 
+# csv파일로 저장 
 def save_to_file(list_lec):
     file = open("univ_all_lec.csv", mode="w", newline='')
     writer = csv.writer(file)
-    writer.writerow(["index", "univ_lec_address", "subject_title"])
+    writer.writerow(["index", "univ_lec_address", "subject_title", "profess_name"])
     for lec in list_lec:
-        writer.writerow(list(lec.values())[0:3])
+        writer.writerow(list(lec.values())[0:4])
     return
 
 
-for i in range(170, 180):
+# 소스코드가 길어져서 함수로 만들어버림.
+def get_text2(c):
+    return c.get_text().strip().replace(u'\xa0', u' ')
+
+
+for i in range(170, 176):
     # 과도한 요청으로 차단 방지
     sleep_time = random.uniform(2, 4)
     sleep(sleep_time)
@@ -58,17 +63,18 @@ for i in range(170, 180):
     soup = BeautifulSoup(url.content, "html.parser")
 
     try:
-        title = soup.find("div", {"class": "sub"}).get_text().strip().replace(u'\xa0', u' ')
-        print(i, univ_url, title)
+        title = get_text2(soup.find("div", {"class": "sub"}))
+        pf_name = get_text2(soup.find("ul", {"class": "info"}).find_all("li")[2])
+        print(i, univ_url, title, pf_name)
 
         lec_list.append({
             "index": i,
             "univ_lec_address": univ_url,
-            "subject_title": title
+            "subject_title": title,
+            "profess_name": pf_name
         })
 
     except:
-        print(f"{i} {univ_url} 정보 없음")
         continue
 
 save_to_file(lec_list)
